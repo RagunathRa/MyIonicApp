b438fc36986df153409efe80c4f0fc338eb7547a
Ragunath Ra <ragunath065@gmail.com>
2023-08-27 00:13:59 +0530

    2

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+interface ContainerProps { }
+
+const Footer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Ready XXXXX  to create an app?</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default Footer;
